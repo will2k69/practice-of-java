54bd214aec3b3366a2799cc2c82fd4d7cbbb9573
will <willamys-g-2001@hotmail.com>
2022-03-19 01:39:37 -0300

    primeira questao

---
+++ 1034.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class code {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        String name = keyboard.nextLine();
+        System.out.printf("Olah, %s!\n", name);
+    }
+}
