7d661b9057be785feaadd50c943bdb0b358b0d44
will <willamys-g-2001@hotmail.com>
2022-03-19 18:13:25 -0300

    add 1152

---
+++ hux_1152.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class hux_1152 {
+    static String vaiPokebola(int n) {
+        if (n == 1) return "Bulbassauro";
+        else if (n == 2) return "Charmander";
+        else return "Squirtle";
+    }
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        int n = keyboard.nextInt();
+        System.out.println(vaiPokebola(n));
+    }
+}
